6a8ebf7dd680d27f4b1d6c5b9b86d04cfa0ff64c
Denis Proskurin <4201995+dpvpro@users.noreply.github.com>
2026-03-30 17:05:24 +0300

    reorganize code

--- main.go
+++ main.go
@@ -9,49 +9,6 @@ import (
 	"strings"
 )
 
-func formatBytes(bytes int64) string {
-	const unit = 1024
-	if bytes < unit {
-		return fmt.Sprintf("%d B", bytes)
-	}
-	div, exp := int64(unit), 0
-	for n := bytes / unit; n >= unit; n /= unit {
-		div *= unit
-		exp++
-	}
-	return fmt.Sprintf("%.2f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
-}
-
-func getExtension(filename string) string {
-	ext := filepath.Ext(filename)
-	if ext == "" {
-		return "no extension"
-	}
-	return strings.ToLower(ext)
-}
-
-func fileHandler(path string, d fs.DirEntry, err error) error {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error accessing %s: %v\n", path, err)
-		return nil
-	}
-
-	if d.IsDir() {
-		return nil
-	}
-
-	info, err := d.Info()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error getting info for %s: %v\n", path, err)
-		return nil
-	}
-
-	ext := getExtension(d.Name())
-	extensionSizes[ext] += info.Size()
-
-	return nil
-}
-
 var extensionSizes = make(map[string]int64)
 
 type ExtensionSize struct {
@@ -68,7 +25,7 @@ func main() {
 	absDir, err := filepath.Abs(dir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error getting absolute path: %v\n", err)
-		absDir = dir
+		os.Exit(1)
 	}
 
 	info, err := os.Stat(dir)
@@ -117,4 +74,47 @@ func main() {
 	fmt.Println(strings.Repeat("-", 70))
 	fmt.Printf("%-40s %15s %9.2f%%\n", "Total", formatBytes(totalSize), 100.0)
 	fmt.Printf("Total extensions found: %d\n", len(results))
+}
+
+func formatBytes(bytes int64) string {
+	const unit = 1024
+	if bytes < unit {
+		return fmt.Sprintf("%d B", bytes)
+	}
+	div, exp := int64(unit), 0
+	for n := bytes / unit; n >= unit; n /= unit {
+		div *= unit
+		exp++
+	}
+	return fmt.Sprintf("%.2f %cB", float64(bytes)/float64(div), "KMGTPE"[exp])
+}
+
+func getExtension(filename string) string {
+	ext := filepath.Ext(filename)
+	if ext == "" {
+		return "no extension"
+	}
+	return strings.ToLower(ext)
+}
+
+func fileHandler(path string, d fs.DirEntry, err error) error {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error accessing %s: %v\n", path, err)
+		return nil
+	}
+
+	if d.IsDir() {
+		return nil
+	}
+
+	info, err := d.Info()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error getting info for %s: %v\n", path, err)
+		return nil
+	}
+
+	ext := getExtension(d.Name())
+	extensionSizes[ext] += info.Size()
+
+	return nil
 }
